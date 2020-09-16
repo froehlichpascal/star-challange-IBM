e7cba6df5edba6f4e8c7a5098dae0632e25d2a58
Pascal Fröhlich <mail@pascalfroehlich.at>
2020-09-16 12:33:17 +0200

    Impl for Task2: Prodaction Service The Test for the Lock is still missing. Due to time.

--- src/main/java/com/ibm/intro/CRUDService.java
+++ src/main/java/com/ibm/intro/CRUDService.java
@@ -8,7 +8,10 @@
  */
 package com.ibm.intro;
 
+import com.ibm.intro.exception.DataStoreException;
 import com.ibm.intro.model.AbstractObject;
+
+import java.util.List;
 
 /**
  * @author Richard Holzeis
@@ -18,7 +21,19 @@ public interface CRUDService<O extends AbstractObject> {
 
 	/*
 	 * #task 2: define me
-	 * 
+	 *
 	 * create, read, update, delete
 	 */
+
+	List<O> findAll();
+
+	O findById(String id);
+
+	List<O> create(List<O> object);
+
+	O create(O object) throws DataStoreException;
+
+	O update(O object) throws DataStoreException;
+
+	void delete(O object) throws DataStoreException;
 }
--- src/test/java/com/ibm/intro/ProductionServiceTest.java
+++ src/test/java/com/ibm/intro/ProductionServiceTest.java
@@ -1,12 +1,26 @@
 /**
  * star-challenge
- *
+ * <p>
  * (C) Copyright 2016 IBM Corporation
  * All rights reserved
- *
+ * <p>
  * Creation date: 03.08.2016
  */
 package com.ibm.intro;
+
+import com.ibm.intro.exception.DataStoreException;
+import com.ibm.intro.model.Production;
+import com.ibm.intro.model.ProductionType;
+import com.ibm.intro.model.Status;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Richard Holzeis
@@ -16,4 +30,86 @@ public class ProductionServiceTest {
 	/*
 	 * #task 2: implement me!
 	 */
+
+
+	@Test
+	public void testSaveAndRead() throws DataStoreException {
+		ProductionService service = new ProductionService();
+		final Production production = new Production(ProductionType.COLLECTION, 1L);
+
+		Production pro = service.create(production);
+
+		Assert.assertEquals(production, service.findById(pro.getId()));
+	}
+
+	@Test
+	public void testReadList() {
+		ProductionService service = new ProductionService();
+		List<Production> productionList = new ArrayList<Production>(Arrays.asList(
+				new Production(ProductionType.COLLECTION, 1L),
+				new Production(ProductionType.DELIVERY, 1L)));
+
+		service.create(productionList);
+
+		Assert.assertTrue("List is Empty", !service.findAll().isEmpty());
+	}
+
+	@Test
+	public void testUpdate() throws DataStoreException {
+		ProductionService service = new ProductionService();
+		final Production production = new Production(ProductionType.COLLECTION, 1L);
+
+		Production pro = service.create(production);
+
+		Assert.assertEquals(ProductionType.COLLECTION, pro.getProductionType());
+
+
+		pro.setProductionType(ProductionType.DELIVERY);
+		service.update(pro);
+
+		Assert.assertEquals(ProductionType.DELIVERY, service.findById(pro.getId()).getProductionType());
+	}
+
+	@Test
+	public void testDelete() throws DataStoreException {
+		ProductionService service = new ProductionService();
+		final Production production = new Production(ProductionType.COLLECTION, 1L);
+
+		Production pro = service.create(production);
+
+		Assert.assertEquals(service.findById(pro.getId()), production);
+
+		service.delete(pro);
+
+		Assert.assertTrue("is still there", service.findById(pro.getId()) == null);
+	}
+
+	@Test
+	public void testLocking() throws InterruptedException, DataStoreException {
+		ProductionService service = new ProductionService();
+		Production production = new Production(ProductionType.COLLECTION, 1L);
+		final Production protest = service.create(production);
+		List<Status> testList = Arrays.asList(Status.CREATED, Status.COMPLETED);
+
+		// when
+		final ExecutorService executor = Executors.newFixedThreadPool(testList.size());
+
+		for (Status testStatus : testList) {
+			executor.execute(() -> {
+				try {
+					protest.setStatus(testStatus);
+					service.update(protest);
+				} catch (DataStoreException dataStoreException) {
+					dataStoreException.printStackTrace();
+				}
+			});
+		}
+
+		executor.shutdown();
+		executor.awaitTermination(1, TimeUnit.MINUTES);
+
+		Assert.assertEquals(Status.COMPLETED, service.findById(protest.getId()).getStatus());
+	}
+
+
 }
