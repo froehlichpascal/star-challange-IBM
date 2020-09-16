8218cc4e082fa55dbf9087aeab68ce6815fc578b
Pascal Fröhlich <mail@pascalfroehlich.at>
2020-09-16 12:56:33 +0200

    leider lässt mein Zeit nicht mehr zu deswegen Task 3 nur als Kommentar.

--- src/main/java/com/ibm/intro/EventService.java
+++ src/main/java/com/ibm/intro/EventService.java
@@ -15,6 +15,19 @@ public class EventService {
 
 	/*
 	 * #task 3: implement me
-	 * 
+	 *
+	 *
+	 * Was würde ich als nächstes Machen:
+	 * Testfälle schreiben so wie sie in der Readme in der Tabellen.
+	 *
+	 * Dann das Service implementieren
+	 * Dabei einen Check einbauen ob die Production auch exestiert.
+	 * Züsätzlich auch noch die Checks das sich der Event Typ nur nach vorne verändern darf.
+	 * Diese fälle Loggen
+	 * Logging mittels Log4J static Logger log = Logger.getLogger(EventService.class.getName())
+	 * logger.warn(  "Mein Log"  );
+	 * Warscheinlich mittels CompletableFuture Asyncrone verarbeitung
+	 *
 	 */
+
 }
